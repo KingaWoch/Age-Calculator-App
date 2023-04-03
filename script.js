162bd0f598421c21b8cc77d41083969483a4585d
Kinga Woch <kinga-niecko@o2.pl>
2023-04-03 12:20:42 +0200

    displaying result

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const inputs = document.querySelectorAll("input");
 const labels = document.querySelectorAll("label");
 const errorMsgs = document.querySelectorAll(".error-msg");
 const form = document.querySelector("form");
+let result = document.querySelectorAll(".result");
+let now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth() + 1; // 0 - January,  11- December
+let day = now.getDate();
+
+let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 form.addEventListener("submit", (e) => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth(); // 0 - January,  11- December
-  const day = now.getDate();
-  console.log(day, month, year);
-
   e.preventDefault();
 
   // day validation
@@ -34,42 +35,51 @@ form.addEventListener("submit", (e) => {
 
   // month validation
 
-  if (!inputs[1].value.match(monthRegEx)) {
+  if (!inputs[1].value) {
+    inputs[1].classList.add("invalid");
+    labels[1].classList.add("invalid");
+    errorMsgs[1].innerHTML = "This field is required";
+  } else if (!inputs[1].value.match(monthRegEx)) {
     inputs[1].classList.add("invalid");
     labels[1].classList.add("invalid");
     errorMsgs[1].innerHTML = "Must be a valid month";
+  } else if (
+    inputs[1].value === "2" &&
+    !inputs[0].value.match(FabruaryDaysRegEx)
+  ) {
+    inputs[0].classList.add("invalid");
+    labels[0].classList.add("invalid");
+    errorMsgs[0].innerHTML = "Must be a valid day";
+  } else if (
+    inputs[1].value === "02" &&
+    !inputs[0].value.match(FabruaryDaysRegEx)
+  ) {
+    inputs[0].classList.add("invalid");
+    labels[0].classList.add("invalid");
+    errorMsgs[0].innerHTML = "Must be a valid day";
   } else {
     inputs[1].classList.remove("invalid");
     labels[1].classList.remove("invalid");
     errorMsgs[1].innerHTML = "";
   }
 
-  // if (inputs[1].value === "2" && !inputs[0].value.match(FabruaryDaysRegEx)) {
-  //   inputs[0].classList.add("invalid");
-  //   labels[0].classList.add("invalid");
-  //   errorMsgs[0].innerHTML = "Must be a valid day";
-  // } else if (
-  //   inputs[1].value === "02" &&
-  //   !inputs[0].value.match(FabruaryDaysRegEx)
-  // ) {
-  //   inputs[0].classList.add("invalid");
-  //   labels[0].classList.add("invalid");
-  //   errorMsgs[0].innerHTML = "Must be a valid day";
-  // } else {
-  //   inputs[0].classList.remove("invalid");
-  //   labels[0].classList.remove("invalid");
-  //   errorMsgs[0].innerHTML = "";
-  // }
-
   // year validation
 
-  if (inputs[2].value > year) {
+  if (!inputs[2].value) {
+    inputs[2].classList.add("invalid");
+    labels[2].classList.add("invalid");
+    errorMsgs[2].innerHTML = "This field is required";
+  } else if (inputs[2].value < 1000) {
+    inputs[2].classList.add("invalid");
+    labels[2].classList.add("invalid");
+    errorMsgs[2].innerHTML = "Must be a valid year";
+  } else if (inputs[2].value > year) {
     inputs[2].classList.add("invalid");
     labels[2].classList.add("invalid");
     errorMsgs[2].innerHTML = "Must be in the past";
   } else if (
     inputs[2].value == year &&
-    inputs[1].value >= month + 1 &&
+    inputs[1].value >= month &&
     inputs[0].value > day
   ) {
     inputs[2].classList.add("invalid");
@@ -80,4 +90,38 @@ form.addEventListener("submit", (e) => {
     labels[2].classList.remove("invalid");
     errorMsgs[2].innerHTML = "";
   }
+
+  if (
+    (inputs[0].value &&
+      inputs[1].value &&
+      inputs[2].value &&
+      inputs[0].value.match(dayRegEx) &&
+      inputs[1].value.match(monthRegEx) &&
+      inputs[2].value > 1000 &&
+      inputs[2].value < year) ||
+    (inputs[2].value == year &&
+      inputs[1].value <= month &&
+      inputs[0].value > day)
+  ) {
+    showResult();
+  }
 });
+
+function showResult() {
+  if (inputs[0].value > day) {
+    day = day + months[month - 1];
+    month = month - 1;
+  }
+
+  if (inputs[1].value > month) {
+    month = month + 12;
+    year = year - 1;
+  }
+
+  let d = day - inputs[0].value;
+  let m = month - inputs[1].value;
+  let y = year - inputs[2].value;
+  result[0].innerHTML = `${y} <span>years</span>`;
+  result[1].innerHTML = `${m} <span>months</span>`;
+  result[2].innerHTML = `${d} <span>days</span>`;
+}
